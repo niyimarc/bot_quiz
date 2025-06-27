0c0eb816b2044679f3677660c0cf35081f05de9c
Drey <oluwadamilareadeniye@yahoo.com>
2025-06-27 22:29:34 +0100

    modified production settings

--- quiz_project/settings/prod.py
+++ quiz_project/settings/prod.py
@@ -2,3 +2,13 @@ from .base import *
 
 DEBUG = False
 ALLOWED_HOSTS = ["www.quiz.fafkiddies.com", "quiz.fafkiddies.com"]
+
+# this is the path to the static folder where css, js and images are stored
+STATIC_DIR = BASE_DIR / '/home/fafkrlee/nanskim_frontend/static/'
+
+STATIC_URL = 'static/'
+STATIC_ROOT = '/home/fafkrlee/quiz.fafkiddies.com/static/'
+
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
