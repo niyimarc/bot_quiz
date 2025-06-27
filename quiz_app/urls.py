c916eabdf2428adf73b0c9fc77c823e1eb7b9f4c
Drey <oluwadamilareadeniye@yahoo.com>
2025-06-27 22:37:29 +0100

    modified webhook url

--- quiz_app/urls.py
+++ quiz_app/urls.py
@@ -3,5 +3,5 @@ from .views import quiz_questions_api, telegram_webhook
 
 urlpatterns = [
     path('quiz/questions/', quiz_questions_api),
-    path("webhook/YOUR_SECRET_PATH/", telegram_webhook, name="telegram-webhook"),
+    path("webhook/bot_webhook/", telegram_webhook, name="telegram-webhook"),
 ]
