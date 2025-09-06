4d32b261971c9e0693765dd0f10b1b06c055354b
Drey <oluwadamilareadeniye@yahoo.com>
2025-09-06 11:08:53 +0100

    set up allowed hosts and static directory for production

--- quiz_project/settings/prod.py
+++ quiz_project/settings/prod.py
@@ -1,13 +1,13 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ["www.quiz.fafkiddies.com", "quiz.fafkiddies.com"]
+ALLOWED_HOSTS = ["www.quizbackend.speedspro.us", "quizbackend.speedspro.us"]
 
 # this is the path to the static folder where css, js and images are stored
-STATIC_DIR = BASE_DIR / '/home/fafkrlee/bot_quiz/static/'
+STATIC_DIR = BASE_DIR / '/home/speebndt/quiz_backend/static/'
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/home/fafkrlee/quiz.fafkiddies.com/static/'
+STATIC_ROOT = '/home/speebndt/quizbackend.speedspro.us/static/'
 
 STATICFILES_DIRS = [
     STATIC_DIR,
